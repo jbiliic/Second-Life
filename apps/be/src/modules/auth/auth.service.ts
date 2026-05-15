749ee8b3c33f185b07bf52873a9e6a487a3967c9
bilic <jopab03@gmail.com>
2026-05-15 21:39:51 +0200

    mail swrvice fix 4

--- apps/be/src/modules/auth/auth.service.ts
+++ apps/be/src/modules/auth/auth.service.ts
@@ -72,18 +72,6 @@ export class AuthService {
             },
         });
 
-        const verificationToken = randomBytes(32).toString('hex');
-        await this.cache.set(
-            `verify:${verificationToken}`,
-            { companyId: company.id } as VerifyPayload,
-            1000 * 60 * 30,
-        );
-        await this.mailService
-            .sendVerificationEmail(company.email, verificationToken)
-            .catch((err) => {
-                console.error('Failed to send verification email:', err);
-            });
-
         const payload = {
             id: company.id,
         } as AuthenticatedUser;
